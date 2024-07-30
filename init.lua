7dee8f8693b40d0062aaf9e1af75e5abf93754f5
Harry Cho <pandar00@hotmail.com>
2024-07-30 09:49:29 -0700

    colorcolumn, faster smooth scrolling

--- init.lua
+++ init.lua
@@ -166,6 +166,10 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Column ruler
+-- https://neovim.io/doc/user/options.html#'colorcolumn'
+vim.opt.colorcolumn = '100'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -967,7 +971,33 @@ require('lazy').setup({
   {
     'karb94/neoscroll.nvim',
     config = function()
-      require('neoscroll').setup()
+      -- Set mappings manually to override scroll duration.
+      -- TODO: Plugin doesn't appear to provide a way to override only the duration.
+      local neoscroll = require 'neoscroll'
+      neoscroll.setup {
+        mappings = {}, -- Do not set any mappings. It's set in the later step.
+        hide_cursor = true,
+        stop_eof = true,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing = 'linear',
+        pre_hook = nil,
+        post_hook = nil,
+        performance_mode = false,
+      }
+      -- Override mappings with custom duration.
+      local mapping_funcs = {
+        ['<C-u>'] = function()
+          neoscroll.ctrl_u { duration = 115 }
+        end,
+        ['<C-d>'] = function()
+          neoscroll.ctrl_d { duration = 115 }
+        end,
+      }
+      local modes = { 'n', 'v', 'x' }
+      for k, v in pairs(mapping_funcs) do
+        vim.keymap.set(modes, k, v)
+      end
     end,
   },
   -- statusbar
