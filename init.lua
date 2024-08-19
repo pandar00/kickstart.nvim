427d2448d4f1b3e15d064f32c5a880f53298cc9f
Harry Cho <pandar00@hotmail.com>
2024-08-19 14:59:56 -0700

    borders around completion

--- init.lua
+++ init.lua
@@ -872,6 +872,10 @@ require('lazy').setup({
             luasnip.lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         completion = { completeopt = 'menu,menuone,noinsert' },
 
         -- For an understanding of why these mappings were
