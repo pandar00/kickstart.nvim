7457e91127ed4860fd64116bde6448c38eb7adb7
Harry Cho <pandar00@hotmail.com>
2024-11-11 08:58:30 -0800

    themes, gitsign

--- init.lua
+++ init.lua
@@ -878,7 +878,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'onedark'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -1062,11 +1062,13 @@ require('lazy').setup({
     },
     keys = {
       -- <leader>c_ : langauge agnostic
-      { '<leader>ct', '<cmd>GoTest -n<cr>', desc = '[C]ode [T]est' },
-      { '<leader>cta', '<cmd>GoTest<cr>', desc = '[C]ode Test [A]ll' },
-      { '<leader>ctp', '<cmd>GoTest -p<cr>', desc = '[C]ode Test Current [P]ackage' },
-      { '<leader>cl', '<cmd>GoLint<cr>', desc = '[C]ode [L]int' },
+      { '<leader>ct', '<cmd>GoTest -n -F<cr>', desc = '[C]ode [T]est' },
+      { '<leader>ctv', '<cmd>GoTest -n -F -v<cr>', desc = '[C]ode [T]est [V]erbose' },
+      { '<leader>cta', '<cmd>GoTest -F<cr>', desc = '[C]ode Test [A]ll' },
+      { '<leader>ctp', '<cmd>GoTest -p -F<cr>', desc = '[C]ode Test Current [P]ackage' },
+      { '<leader>cl', '<cmd>GoLint -F<cr>', desc = '[C]ode [L]int' },
       { '<leader>cd', '<cmd>GoDebug<cr>', desc = '[C]ode [D]ebug' },
+      { '<leader>cds', '<cmd>GoDbgStop<cr>', desc = '[C]ode [D]ebug [S]top' },
 
       -- <leader>g_
       { '<leader>gf', '<cmd>GoFillStruct<cr>', desc = '[G]o [F]ill struct' },
@@ -1481,6 +1483,13 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'navarasu/onedark.nvim',
+    opts = {
+      style = 'deep',
+    },
+  },
+
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
   --
   --  Here are some example plugins that I've included in the Kickstart repository.
@@ -1491,7 +1500,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
