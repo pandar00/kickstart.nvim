ab83a0f75f0c923e525bf09e9c5e9177c98b43a2
Harry Cho <pandar00@hotmail.com>
2026-03-14 10:08:15 -0700

    avante: enable

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -9,36 +9,68 @@ return {
   ---@type avante.Config
   opts = {
     -- add any opts here
+    -- this file can contain specific instructions for your project
+    instructions_file = "avante.md",
     -- for example
-    provider = "claude",
+    -- mode = "legacy",
+    provider = "openai",
     providers = {
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-sonnet-4-20250514",
-        timeout = 30000, -- Timeout in milliseconds
-        extra_request_body = {
-          temperature = 0.75,
-          max_tokens = 20480,
-        },
+      -- ["mimo-v2-flash"] = {
+      --   endpoint = "https://api.xiaomimimo.com/v1/chat/completions", -- The full endpoint of the provider
+      --   model = "mimo-v2-flash", -- The model name to use with this provider
+      --   api_key_name = "AVANTE_XIAOMI_FLASH_API_KEY", -- The name of the environment variable that contains the API key
+      --   ---@type fun(opts: AvanteProvider, code_opts: AvantePromptOptions): AvanteCurlOutput
+      --   parse_curl_args = function(opts, code_opts) end,
+      --   --- This function will be used to parse incoming SSE stream
+      --   --- It takes in the data stream as the first argument, followed by SSE event state, and opts
+      --   --- retrieved from given buffer.
+      --   --- This opts include:
+      --   --- - on_chunk: (fun(chunk: string): any) this is invoked on parsing correct delta chunk
+      --   --- - on_complete: (fun(err: string|nil): any) this is invoked on either complete call or error chunk
+      --   ---@type fun(data_stream: string, event_state: string, opts: ResponseParser): nil
+      --   parse_response = function(data_stream, event_state, opts) end,
+      --   --- The following function SHOULD only be used when providers doesn't follow SSE spec [ADVANCED]
+      --   --- this is mutually exclusive with parse_response_data
+      --   ---@type fun(data: string, handler_opts: AvanteHandlerOptions): nil
+      --   parse_stream_data = function(data, handler_opts) end,
+      -- },
+      openai = {
+        endpoint = "https://api.xiaomimimo.com/v1",
+        auth_type = "api", -- Set to "max" to sign in with Claude Pro/Max subscription
+        model = "mimo-v2-flash",
+        -- extra_request_body = {
+        --   temperature = 0.75,
+        --   max_tokens = 4096,
+        -- },
       },
-      moonshot = {
-        endpoint = "https://api.moonshot.ai/v1",
-        model = "kimi-k2-0711-preview",
-        timeout = 30000, -- Timeout in milliseconds
-        extra_request_body = {
-          temperature = 0.75,
-          max_tokens = 32768,
-        },
-      },
+      -- claude = {
+      --   endpoint = "https://api.anthropic.com",
+      --   model = "claude-sonnet-4-20250514",
+      --   timeout = 30000, -- Timeout in milliseconds
+      --   extra_request_body = {
+      --     temperature = 0.75,
+      --     max_tokens = 20480,
+      --   },
+      -- },
+      -- moonshot = {
+      --   endpoint = "https://api.moonshot.ai/v1",
+      --   model = "kimi-k2-0711-preview",
+      --   timeout = 30000, -- Timeout in milliseconds
+      --   extra_request_body = {
+      --     temperature = 0.75,
+      --     max_tokens = 32768,
+      --   },
+      -- },
     },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-mini/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "saghen/blink.cmp",
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "stevearc/dressing.nvim", -- for input provider dressing
     "folke/snacks.nvim", -- for input provider snacks
@@ -61,16 +93,13 @@ return {
         },
       },
     },
-    -- WARN: render-markdown conflicts with obsidian
     {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
-        -- file_types = { 'markdown', 'Avante' },
-        file_types = { "Avante" },
+        file_types = { "markdown", "Avante" },
       },
-      -- ft = { 'markdown', 'Avante' },
-      ft = { "Avante" },
+      ft = { "markdown", "Avante" },
     },
   },
 }
--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -312,6 +312,7 @@ return { -- Autocompletion
   -- https://cmp.saghen.dev/installation#lazy-nvim
   -- build = "cargo build --release",
   dependencies = {
+    "Kaiser-Yang/blink-cmp-avante",
     -- Snippet Engine
     {
       "L3MON4D3/LuaSnip",
@@ -451,19 +452,19 @@ return { -- Autocompletion
     },
 
     sources = {
-      default = { "lsp", "path", "snippets", "lazydev" },
+      default = { "avante", "lsp", "path", "snippets", "lazydev" },
       per_filetype = {
         -- don't want AI assistance in markdown
         markdown = { "lsp", "path", "snippets", "lazydev" },
       },
       providers = {
-        -- avante = {
-        --   module = 'blink-cmp-avante',
-        --   name = 'Avante',
-        --   opts = {
-        --     -- options for blink-cmp-avante
-        --   },
-        -- },
+        avante = {
+          module = "blink-cmp-avante",
+          name = "Avante",
+          opts = {
+            -- options for blink-cmp-avante
+          },
+        },
         -- NOTE: higher to score, the more likely it appears top
         lazydev = {
           module = "lazydev.integrations.blink",
