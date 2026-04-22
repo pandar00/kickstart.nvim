39f75e43135dfaeac5f4295c9a7d9bbfe28b6c4d
Harry Cho <pandar00@hotmail.com>
2026-04-21 21:25:33 -0700

    remove(golines) from go formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -36,7 +36,7 @@ return {
     -- yamlls, for example, has yaml.format.enable config
     formatters_by_ft = {
       lua = { "stylua" },
-      go = { "gofumpt", "goimports", "golines" },
+      go = { "gofumpt", "goimports" },
       sh = { "shellcheck", "shfmt" },
       -- Conform can also run multiple formatters sequentially
       python = { "autoflake", "isort", "black" },
