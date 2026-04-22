68cf2364d41e39ebcdfddfaaa4fcc084b64bd907
Harry Cho <pandar00@hotmail.com>
2026-04-21 18:37:05 -0700

    refactor(telescope): simplify current buffer fuzzy find keymap
    The <leader>/ keymap was wrapped in a function to apply a dropdown theme with
    previewer disabled. Since the default current_buffer_fuzzy_find behavior is
    sufficient for the intended use case, the configuration is now passed directly.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -123,13 +123,14 @@ return {
     vim.keymap.set("n", "<leader>so", ":Obsidian search<CR>", { desc = "[S]earch [O]bsidian" })
     vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-    vim.keymap.set("n", "<leader>/", function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-        winblend = 10,
-        previewer = false,
-      }))
-    end, { desc = "[/] Fuzzily search in current buffer" })
+    vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search in current buffer" })
+    -- vim.keymap.set("n", "<leader>/", function()
+    --   -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+    --   builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    --     winblend = 10,
+    --     previewer = true,
+    --   }))
+    -- end, { desc = "[/] Fuzzily search in current buffer" })
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
